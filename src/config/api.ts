b833f7251ffebc35254638b77c9caf107d8cce0d
proSamik <samikchoudhury15@gmail.com>
2025-01-15 00:36:16 +0530

    feat- create api endpoint configuration in api.ts (src/config)

---
+++ src/config/api.ts
@@ -0,0 +1,8 @@
+// config/api.ts
+export const config = {
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:10000',
+    port: process.env.NEXT_PUBLIC_PORT || 10000,
+    apiEndpoints: {
+        md: '/md',
+    }
+};
