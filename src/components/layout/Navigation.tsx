09ef53ce68c924c9a37e70ec45a5300e27dedf9a
proSamik <samikchoudhury15@gmail.com>
2025-01-16 01:29:51 +0530

    fix- update Navigation.tsx to have fixed width at certain times

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -5,10 +5,14 @@ import ThemeToggle from '@/components/layout/ThemeToggle';
 
 export default function Navigation() {
     return (
-        <div className="fixed top-0 left-0 right-0 bg-white dark:bg-dark-background z-50 flex justify-center">
+        <div className="fixed top-0 left-0 right-0 bg-white dark:bg-dark-background z-50 flex justify-center w-full mx-auto
+         sm:max-w-screen-md
+         md:max-w-screen-lg ">
+
             <nav
-                className="flex w-full max-w-4xl px-3 border-b border-l border-r border-amber-200 dark:border-amber-700 rounded-b-lg">
-                <div className="flex justify-between items-center h-16 w-full">
+                className="flex w-full px-3 border-b border-l border-r border-amber-200 dark:border-amber-700 rounded-b-lg">
+                <div className="flex justify-center items-center w-full h-16">
+                    {/* Logo Section */}
                     <Link
                         href="/"
                         className="relative w-16 h-16"
@@ -22,7 +26,8 @@ export default function Navigation() {
                             className="object-contain invert dark:invert-0"
                         />
                     </Link>
-                    <div className="flex items-center space-x-6">
+                    {/* Navigation Links */}
+                    <div className="flex items-center space-x-6 ml-auto mr-5">
                         <div className="flex items-center space-x-4">
                             <Link href="/"
                                   className="text-amber-800 dark:text-amber-200 hover:text-amber-600 dark:hover:text-amber-400 transition-colors">
