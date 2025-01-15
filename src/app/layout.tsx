94c5db115fe78a55c3f004f95741b2fcebd85e55
proSamik <samikchoudhury15@gmail.com>
2025-01-16 01:30:02 +0530

    fix- update layout.tsx to have fixed width at certain times

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,14 +19,21 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" suppressHydrationWarning>
-        <body className={`${inter.className} bg-white dark:bg-dark-background text-zinc-900 dark:text-white transition-colors min-h-screen flex flex-col`}>
+        <body
+            className={`${inter.className} bg-white dark:bg-dark-background text-zinc-900 dark:text-white transition-colors min-h-screen flex flex-col`}>
         <Providers>
             <ErrorBoundary>
-                <Navigation />
-                <main className="container mx-auto px-4 py-8 max-w-4xl mt-8">
+                <Navigation/>
+                <main className="container
+                px-4 py-8
+                w-full
+                mx-auto
+                sm:max-w-screen-md
+                md:max-w-screen-lg
+                mt-8">
                     {children}
                 </main>
-                <Footer />
+                <Footer/>
             </ErrorBoundary>
         </Providers>
         </body>
