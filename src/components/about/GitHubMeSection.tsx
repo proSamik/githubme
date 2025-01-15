06d15710bcf74d05bc03844b1a88a4791a36e782
proSamik <samikchoudhury15@gmail.com>
2025-01-15 21:05:44 +0530

    feat- update GitHubMeSection.tsx to use logo instead of text to show about GitHubMe Section

--- src/components/about/GitHubMeSection.tsx
+++ src/components/about/GitHubMeSection.tsx
@@ -1,16 +1,23 @@
 // components/GitHubMeSection.tsx
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import FeatureGrid from '@/components/about/FeatureGrid';
 
 export default function GitHubMeSection() {
     return (
         <div className="space-y-6 animate-fadeIn">
-            <div className="text-center">
-                <h1 className="text-4xl font-bold text-amber-800 dark:text-amber-200">
-                    GitHubMe
-                </h1>
-                <p className="text-xl text-gray-600 dark:text-gray-300">
+            <div className="text-center flex flex-col items-center">
+                <div className="relative w-32 h-32">
+                    <Image
+                        src="/logo-crop.png"
+                        alt="GithubMe Logo"
+                        fill
+                        priority
+                        className="object-contain invert dark:invert-0"
+                    />
+                </div>
+                <p className="text-3xl font-bold bg-gradient-to-r from-amber-700 to-amber-500 dark:from-amber-400 dark:to-amber-200 bg-clip-text text-transparent">
                     A Stylish Markdown Viewer
                 </p>
             </div>
@@ -27,7 +34,7 @@ export default function GitHubMeSection() {
                     dark:hover:from-amber-900/50 dark:hover:to-amber-800/70
                 ">
                     <h2 className="text-2xl font-semibold text-amber-800 dark:text-amber-200 mb-2 flex justify-center">
-                        The Story
+                    The Story
                     </h2>
                     <p className="text-gray-700 dark:text-gray-300 leading-relaxed">
                         GitHubMe is a brainchild of {' '}
