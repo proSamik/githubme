4c46a97696dfc1a04672517b70d1025838611554
proSamik <samikchoudhury15@gmail.com>
2025-01-18 02:31:15 +0530

    feat- add new feature in Code category and update text

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react';
-import {ArrowRight, Camera, FileCode, Code2, GitBranch, ImageIcon} from 'lucide-react';
+import {ArrowRight, Camera, FileCode, Code2, GitBranch, ImageIcon, Copy} from 'lucide-react';
 import { TypingDemo } from '@/components/landing/auto-demo/TypingDemo';
 import { FaGithub } from "react-icons/fa";
 import QuickSearch from "@/components/QuickSearch";
@@ -55,25 +55,35 @@ export default function HeroSection() {
             max-w-screen-lg
             mb-2
         ">
-            <div className="pt-1 mt-4 mb-2">
+            <div className="pt-1 sm:mt-4 mb-2">
                 <QuickSearch/>
             </div>
 
-            <div className="flex-1 flex flex-col my-auto items-center space-y-6 pt-5 w-full md:space-y-10 lg:space-y-16">
+            <div
+                className="flex-1 flex flex-col my-auto items-center space-y-6 pt-5 w-full md:space-y-8 lg:space-y-12">
                 <h1 className="font-bold text-center text-wrap">
                     <AnimatedTitle/>
                 </h1>
 
-                <p className="text-[16px] md:text-2xl text-center text-gray-600 dark:text-gray-300 text-wrap max-w-3xl">
-                    Transform boring Markdown README files into Technical Blog, Product Documentation or Personal Notes.
-                </p>
+                <div className="flex flex-col space-y-2 lg:space-y-4">
+                    <p className="text-[16px] md:text-2xl font-bold text-center text-amber-600 dark:text-gray-300 text-wrap max-w-3xl">
+                        One Stop Place for Markdown Content Viewer
+                    </p>
+
+                    <p className="text-[16px] md:text-2xl text-center text-gray-600 dark:text-gray-300 text-wrap max-w-3xl">
+                        Transform boring Markdown README files into Technical Blog, Product Documentation or Personal
+                        Notes.
+                    </p>
+                </div>
+
 
                 <div className="flex flex-col gap-8 lg:gap-10 max-w-4xl w-full">
                     {/* Creation & Conversion Features */}
                     <div className="space-y-4">
-                        <h3 className="text-lg font-semibold text-amber-800 dark:text-amber-400 text-center">Markdown with Extra Features</h3>
+                        <h3 className="text-lg font-semibold text-amber-800 dark:text-amber-400 text-center">Markdown
+                            with Extra Features</h3>
                         <div className="flex flex-wrap justify-center gap-4">
-                            <div
+                        <div
                                 className="min-w-fit bg-white dark:bg-dark-background dark:shadow-amber-200 dark:shadow-sm shadow-lg px-3 py-2.5 rounded-full flex items-center justify-center gap-3">
                                 <FaGithub size={20} className="text-amber-600 dark:text-amber-400"/>
                                 <span>Instant Conversion</span>
@@ -102,18 +112,24 @@ export default function HeroSection() {
                     {/* Content & Sharing Features */}
                     <div className="space-y-4">
                         <h3 className="text-lg font-semibold text-amber-800 dark:text-amber-400 text-center">Code
-                            Sharing</h3>
+                            Specific</h3>
                         <div className="flex flex-wrap justify-center gap-4">
                             <div
                                 className="min-w-fit bg-white dark:bg-dark-background dark:shadow-amber-200 dark:shadow-sm shadow-lg px-3 py-2.5 rounded-full flex items-center justify-center gap-3">
-                                <FileCode size={20} className="text-amber-600 dark:text-amber-400"/>
+                                <Copy size={20} className="text-amber-600 dark:text-amber-400"/>
                                 <span>Code Copy</span>
                             </div>
 
                             <div
                                 className="min-w-fit bg-white dark:bg-dark-background dark:shadow-amber-200 dark:shadow-sm shadow-lg px-3 py-2.5 rounded-full flex items-center justify-center gap-3">
                                 <Camera size={20} className="text-amber-600 dark:text-amber-400"/>
-                                <span>Code Screenshot</span>
+                                <span>Screenshot png</span>
+                            </div>
+
+                            <div
+                                className="hidden lg:visible min-w-fit bg-white dark:bg-dark-background dark:shadow-amber-200 dark:shadow-sm shadow-lg px-3 py-2.5 rounded-full lg:flex items-center justify-center gap-3">
+                                <FileCode size={20} className="text-amber-600 dark:text-amber-400"/>
+                                <span>Syntax Highlighter</span>
                             </div>
                         </div>
                     </div>
