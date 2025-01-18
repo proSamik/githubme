c1972778bbb27786c9159064fcbd42dc483ce15b
proSamik <samikchoudhury15@gmail.com>
2025-01-18 19:00:37 +0530

    feat- add terms of service

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 // src/components/Footer.tsx
-import {FaGithub, FaLinkedin, FaTwitter} from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export default function Footer() {
     return (
@@ -11,52 +11,61 @@ export default function Footer() {
             border-t border-l border-r border-amber-200 dark:border-amber-700 rounded-t-lg">
                 <div className="px-4 py-2">
                     <div className="flex flex-col-reverse sm:flex-row justify-between items-center gap-4">
-                        <div>
-                            <p className="text-sm text-amber-800 dark:text-amber-200 ">
+                        <div className="flex flex-col items-center justify-center sm:items-start">
+                            <p className="text-sm text-amber-800 dark:text-amber-200">
                                 © {new Date().getFullYear()} GithubMe. All rights reserved.
                             </p>
-                        </div>
-                        <div className="flex space-x-6">
                             <a
+                                href="/prosamik/githubme/blob/feat-tos-and-seo/terms-of-service.md"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-sm text-amber-800 dark:text-amber-200 underline hover:text-amber-600 dark:hover:text-amber-400 mt-1"
+                            >
+                                Terms of Service
+                            </a>
+                    </div>
+
+                    <div className="flex space-x-6">
+                        <a
                             href="https://www.linkedin.com/in/prosamik"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="group flex flex-col items-center text-amber-800 dark:text-amber-200
-                            hover:text-amber-600 dark:hover:text-amber-400 transition-colors"
+                                className="group flex flex-col items-center text-amber-800 dark:text-amber-200
+                                hover:text-amber-600 dark:hover:text-amber-400 transition-colors"
                             >
                             <FaLinkedin size={24} />
-                            <span className="mt-1 text-xs">
-                                    proSamik
-                            </span>
-                        </a>
-                        <a
-                        href="https://www.twitter.com/prosamik"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="group flex flex-col items-center text-amber-800 dark:text-amber-200
-                        hover:text-amber-600 dark:hover:text-amber-400 transition-colors"
-                        >
-                        <FaTwitter size={24} />
-                        <span className="mt-1 text-xs">
+                                <span className="mt-1 text-xs">
                                     proSamik
                                 </span>
-                    </a>
-                    <a
-                    href="https://www.github.com/prosamik"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="group flex flex-col items-center text-amber-800 dark:text-amber-200
-                    hover:text-amber-600 dark:hover:text-amber-400 transition-colors"
-                    >
-                    <FaGithub size={24} />
-                    <span className="mt-1 text-xs">
+                            </a>
+                            <a
+                                href="https://www.twitter.com/prosamik"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="group flex flex-col items-center text-amber-800 dark:text-amber-200
+                                hover:text-amber-600 dark:hover:text-amber-400 transition-colors"
+                            >
+                                <FaTwitter size={24} />
+                                <span className="mt-1 text-xs">
                                     proSamik
                                 </span>
-                </a>
-            </div>
+                            </a>
+                            <a
+                                href="https://www.github.com/prosamik"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="group flex flex-col items-center text-amber-800 dark:text-amber-200
+                                hover:text-amber-600 dark:hover:text-amber-400 transition-colors"
+                            >
+                                <FaGithub size={24} />
+                                <span className="mt-1 text-xs">
+                                    proSamik
+                                </span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </footer>
-);
+    );
 }
