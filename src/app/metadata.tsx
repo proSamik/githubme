c147d12ee4ab80a56b7ef5d2e3b72c79e808488e
proSamik <samikchoudhury15@gmail.com>
2025-01-19 02:09:30 +0530

    feat- delete metadata.tsx

--- src/app/metadata.tsx
+++
@@ -1,6 +0,0 @@
-import type {Metadata} from "next";
-
-export const metadata: Metadata = {
-    title: 'GitHubMe',
-    description: 'Transform GitHub Markdown README files into Technical Blog, Product Documentation or Personal Notes with built-in SEO support.',
-};
