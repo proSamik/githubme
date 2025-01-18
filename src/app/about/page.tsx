289951fc81e64dc2d5899ced9db252759fe5e741
proSamik <samikchoudhury15@gmail.com>
2025-01-18 08:03:05 +0530

    feat- add Page Analytics function in about page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,13 @@
 // app/about/page.tsx
 import React from 'react';
 import AboutContent from '@/components/about/AboutContent';
+import {trackPageView} from "@/lib/api";
 
 export default function AboutPage() {
+
+    // Track page view
+    void trackPageView('githubme_about');
+
     return (
         <main className="flex min-h-screen flex-col items-center  px-4">
             <AboutContent />
